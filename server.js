fe304f32f95cacb716820142256551c2e0594ab4
degrecci <henrique.degrecci@upx.com>
2020-07-07 23:44:05 -0300

    object types

--- server.js
+++ server.js
@@ -3,27 +3,43 @@ const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
   type Query {
     quoteOfTheDay: String,
     random: Float!,
-    rollDice(numDice: Int!, numSides: Int): [Int]
+    getDie(numSides: Int): RandomDie
   }
 `);
+
+class RandomDie {
+  constructor(numSides){
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({ numRolls }) {
+    let output = [];
+    for (let i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
+    }
+    return output;
+  }
+}
 
 const root = {
   quoteOfTheDay: () => {
     return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
   },
   random: () => Math.random(),
-  rollDice: ({ numDice, numSides}) => {
-    let output = [];
-
-    for (let i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)))
-    }
-
-    return output;
-  },
+  getDie: ({ numSides }) => new RandomDie(numSides || 6),
 };
 
 const app = express();
